662834edc97f5de2dceb90b3e283268dde1b7754
Piyush Kumar Singh <142776115+strawhat-90@users.noreply.github.com>
2024-01-09 22:16:24 +0530

    Add files via upload

---
+++ 4_seprateTuple.py
@@ -0,0 +1,27 @@
+# t=('gohan','kudasaie','king','mizu','konichiwa')
+# a=t[0]
+# b=t[1]
+# c=t[2]
+# d=t[3]
+# e=t[4]
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+
+
+
+
+
+# l=[(),(),(),('hihuihui',),('juo',)] 
+
+# a=[] 
+
+
+# for i in l:
+#     a.append(list(i))
+
+# print(a)        
+
+
