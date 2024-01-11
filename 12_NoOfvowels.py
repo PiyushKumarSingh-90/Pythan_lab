898b0e6f924045438075fa24ed672c7682155021
Piyush Kumar Singh <142776115+strawhat-90@users.noreply.github.com>
2024-01-11 22:53:20 +0530

    Add files via upload

---
+++ 12_NoOfvowels.py
@@ -0,0 +1,22 @@
+# 1. wap to count number of vowels in a given string considering both upper case and 
+#    lower case.
+
+# l=['a','e','i','o','u','A','E','I','O','U']
+# f=0
+# str=input("Enter the string : ")
+# for i in str:
+#     if i in l :
+#         f+=1
+# print(f)
+
+
+#  2. wap that takes a sentence as input and counts number of words in it.
+
+w=input("Enter the sentance : ")
+
+list=w.split()
+# print(list)
+
+count=len(list)
+print(count)
+
